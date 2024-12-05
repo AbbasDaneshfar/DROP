e3ad3dfd171fe89bac93fffe26317db7f3ad724c
Abbas Daneshfar <daneshfarabbas@gmail.com>
2024-12-06 00:35:08 +0330

    Implementation monorepo-voting-app in gitlab

---
+++ Exercise-19/psql-test/psql-test.sh
@@ -0,0 +1,5 @@
+cat psql-test/psql-test.sh
+#!/bin/bash
+PGPASSWORD=${POSTGRES_PASSWORD} psql --username=${POSTGRES_USER} -f /opt/psql-test/postgres_backup_*
+PGPASSWORD=${POSTGRES_PASSWORD} psql --username=${POSTGRES_USER} -t -c "SELECT COUNT(*) FROM votes" | sed -n '1 p' | cut -d ' ' -f2 > /opt/psql-test/restore.log
+
